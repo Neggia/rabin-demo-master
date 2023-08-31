404a7bd0de95e1fc79d7e118d0988678d30d9b18
Neggia <test@abc..com>
2023-08-31 16:26:52 +0200

    Added numberToByteString

--- tests/rabinDemo.test.ts
+++ tests/rabinDemo.test.ts
@@ -3,12 +3,57 @@ import { RabinDemo } from '../src/contracts/rabinDemo'
 import { getDefaultSigner } from './utils/txHelper'
 import chaiAsPromised from 'chai-as-promised'
 import axios from 'axios'
-import { bsv } from 'scrypt-ts'
+import { bsv, toByteString, Utils } from 'scrypt-ts'
 import { RabinVerifierWOC, RabinSig } from 'scrypt-ts-lib'
 import { Rabin, RabinSignature } from 'rabinsig'
 use(chaiAsPromised)
 
 const witnessServer = 'https://witnessonchain.com/v1'
+
+interface numberToByteStringOptions {
+    endian?: string; // big / little
+    length?: number;
+  }
+
+function numberToByteString(number, options: numberToByteStringOptions = {}) {
+    if (number < 0) {
+      throw new Error('Number must be non-negative.');
+    }
+  
+    const { endian = 'big', length = undefined } = options;
+  
+    // Calculate the number of bytes needed to represent the number
+    const numBytes = Math.ceil(Math.log2(number + 1) / 8);
+  
+    // Create an array to store the bytes
+    const bytes = new Array(numBytes);
+  
+    // Extract and store each byte
+    for (let i = numBytes - 1; i >= 0; i--) {
+      bytes[i] = number & 0xff; // Extract the least significant byte
+      number >>= 8; // Shift right by 8 bits to get the next byte
+    }
+  
+    // Convert the byte array to a ByteString
+    const byteString = Buffer.from(bytes).toString('hex');
+    let endianByteString = byteString; //endian === 'big' ? byteString : byteString.split('').reverse().join('');
+    if(endian !== 'big'){
+        endianByteString = '';
+        for (let i = byteString.length; i > 0 ; i = i-2) {
+            const byteHexString = byteString.slice(i-2,i);  
+            // byteHexString = byteHexString.slice(1,2) + byteHexString.slice(0,1);   
+            endianByteString = endianByteString + byteHexString;
+        }
+    }
+
+    // Pad the ByteString with leading zeros if a specific length is requested
+    if (length && length > numBytes * 2) {
+      const padding = '0'.repeat(length - numBytes * 2);
+      return endian === 'big' ? padding + endianByteString : endianByteString + padding;
+    }
+  
+    return endianByteString;
+  }
 
 describe('Test SmartContract `RabinDemo`', () => {
     let instance: RabinDemo
@@ -38,6 +83,44 @@ describe('Test SmartContract `RabinDemo`', () => {
         const msg = RabinVerifierWOC.parseMsg(response)
         const sig = RabinVerifierWOC.parseSig(response) */
 
+        const byteString = toByteString('258',true);
+        console.log(`byteString: ${byteString}`); //byteString: 323538
+        const byteString2 = BigInt(Buffer.from('323538', 'hex').toString('ascii'));
+        console.log(`byteString2: ${byteString2}`); //byteString2: 258
+        const byteString3 = BigInt(Buffer.from('258').toString('ascii'));
+        console.log(`byteString3: ${byteString3}`); //byteString3: 258
+        const byteString4 = toByteString('0201',true);
+        console.log(`byteString4: ${byteString4}`); //byteString4: 30323031
+        const leuint = Utils.fromLEUnsigned('02010000');
+        console.log(`leuint: ${leuint}`); //leuint: 258
+        const leuint2 = Utils.fromLEUnsigned('00000201');
+        console.log(`leuint2: ${leuint2}`); //leuint2: 16908288
+        const leuint3 = Utils.fromLEUnsigned('323538');
+        console.log(`leuint3: ${leuint3}`); //leuint3: 3683634
+        const leuint4 = Utils.fromLEUnsigned('0201');
+        console.log(`leuint4: ${leuint4}`); //leuint4: 258
+        const leuint5 = Utils.fromLEUnsigned('0102');
+        console.log(`leuint5: ${leuint5}`); //leuint5: 513
+        const leuint6 = Utils.fromLEUnsigned('20100000');
+        console.log(`leuint6: ${leuint6}`); //leuint6: 4128
+
+        const number = 258;
+        const byteStringBuf = numberToByteString(number);
+        console.log(`byteStringBuf: ${byteStringBuf}`);  // Output: "0102"
+
+        const options = { endian: 'little', length: 8 }; // Little-endian and 8-character ByteString
+        const byteStringBuf2 = numberToByteString(number, options);
+        console.log(`byteStringBuf2: ${byteStringBuf2}`); // byteStringBuf2: 02010000
+
+        /* const number = 258;
+        // Create a Buffer with a single byte
+        const buffer = Buffer.alloc(1);
+        // Write the number as a 8-bit unsigned integer to the Buffer
+        buffer.writeUInt8(number, 0);
+        // Convert the Buffer to a ByteString
+        const byteStringBuf = buffer.toString('hex');
+        console.log(`byteStringBuf: ${byteStringBuf}`); // RangeError [ERR_OUT_OF_RANGE]: The value of "value" is out of range. It must be >= 0 and <= 255. Received 258
+ */
         const digest = "30303030303033313136303030303139323139383030303030303031363933323632303430333034303030303030303136393332363230343033303431"
         const securityLevel = 6; //from 1(512bit) to 6(3072bit) or more
         const rabin = new Rabin(securityLevel);
